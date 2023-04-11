fd8ca90c34c5e8dfcd54514959b098feb4c24076
Robin Lenaerts <contact@robinlenaerts.dev>
2023-04-11 09:35:24 +0200

    updated tests

---
+++ DotnetHeadStart.Test/ModelTests.cs
@@ -0,0 +1,53 @@
+
+
+using Microsoft.Extensions.Options;
+using Serilog;
+
+namespace DotnetHeadStart.Test;
+
+public class ModelTests 
+{
+    readonly DataBaseContext _dataBaseContext;
+    public ProfessionalExperience ProfessionalExperience { get; set; }
+
+    public ModelTests() {
+         _dataBaseContext = new DataBaseContext(new DbContextOptionsBuilder<DataBaseContext>().UseInMemoryDatabase("test").Options, new Mock<ILogger>().Object);
+         ProfessionalExperience = new ProfessionalExperience
+        {
+            Company = "Test Company",
+            Title = "Test Title",
+            StartDate = DateTime.Now.AddMonths(-30),
+            EndDate = DateTime.Now,
+            Description = "Test Description"
+        };
+        
+    }
+
+    [Fact]
+    public void CreatedAdFilledInWhenNewBaseModelIsSaved()
+    {
+        _dataBaseContext.ProfessionalExperiences.Add(ProfessionalExperience);
+        _dataBaseContext.SaveChanges();
+        Assert.NotEqual(DateTime.MinValue, ProfessionalExperience.CreatedAt);
+    }
+
+    [Fact]
+    public void ModifiedAtFilledInWhenBaseModelIsUpdated()
+    {
+        _dataBaseContext.ProfessionalExperiences.Add(ProfessionalExperience);
+        _dataBaseContext.SaveChanges();
+        ProfessionalExperience.Title = "Updated Title";
+        _dataBaseContext.SaveChanges();
+        Assert.NotEqual(DateTime.MinValue, ProfessionalExperience.ModifiedAt);
+    }
+
+    [Fact]
+    public void DeletedAtFilledInWhenBaseModelIsDeleted()
+    {
+        _dataBaseContext.ProfessionalExperiences.Add(ProfessionalExperience);
+        _dataBaseContext.SaveChanges();
+        _dataBaseContext.ProfessionalExperiences.Remove(ProfessionalExperience);
+        _dataBaseContext.SaveChanges();
+        Assert.NotEqual(DateTime.MinValue, ProfessionalExperience.DeletedAt);
+    }
+}
--- DotnetHeadStart.Test/Usings.cs
+++ DotnetHeadStart.Test/Usings.cs
@@ -1,1 +1,6 @@
 global using Xunit;
+global using DotnetHeadStart.Data;
+global using DotnetHeadStart.Models;
+global using Microsoft.EntityFrameworkCore;
+global using Moq;
+global using Moq.EntityFrameworkCore;
