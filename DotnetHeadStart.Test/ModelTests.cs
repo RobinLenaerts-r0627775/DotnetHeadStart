a893b916b46492a4be565bb37b9dfed2069817bc
Robin Lenaerts <r.lenaerts@easi.net>
2023-09-22 14:26:17 +0200

    0 warnings

--- DotnetHeadStart.Test/ModelTests.cs
+++ DotnetHeadStart.Test/ModelTests.cs
@@ -41,8 +41,8 @@ public class BaseModelTests
 
 public class TestObject : BaseModel
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
 }
 
 public class TestDbContext : HeadStartContext
