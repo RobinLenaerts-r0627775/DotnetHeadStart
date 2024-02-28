c5fe07c5a49c206affe39cc55e612be2620e09d5
Robin Lenaerts <r.lenaerts@easi.net>
2024-02-28 14:21:31 +0100

    Add MimeKit and MailKit packages, and implement SMTPMailSender class

---
+++ DotnetHeadStart/Exceptions/HeadStartException.cs
@@ -0,0 +1,13 @@
+﻿namespace DotnetHeadStart;
+
+public class HeadStartException : Exception
+{
+
+    public HeadStartException(string message) : base(message)
+    {
+    }
+
+    public HeadStartException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
---
+++ DotnetHeadStart/Mailing/SMTPMailSender.cs
@@ -0,0 +1,48 @@
+﻿namespace DotnetHeadStart;
+
+public class SMTPMailSender(IConfiguration configuration, ILogger logger) : IEmailSender
+{
+    public readonly IConfiguration _configuration = configuration;
+    public readonly ILogger _logger = logger;
+
+    /// <summary>
+    /// Send email using SMTP
+    /// </summary>
+    /// <param name="recipients">Array of recipients for the email.</param>
+    /// <param name="subject">Subject of the email.</param>
+    /// <param name="htmlMessage">Body of the email.</param>
+    /// <returns> </returns>
+    /// <exception cref="HeadStartException">Thrown when an error occurs while sending the email.</exception>
+    public Task SendEmailAsync(string[] recipients, string subject, string htmlMessage)
+    {
+        var message = new MimeMessage();
+        message.From.Add(MailboxAddress.Parse(_configuration["SMTP_USER"]));
+        foreach (var recipient in recipients)
+        {
+            message.To.Add(MailboxAddress.Parse(recipient));
+        }
+        message.Subject = subject;
+        message.Body = new TextPart(TextFormat.Html) { Text = htmlMessage };
+        try
+        {
+            //send email
+            using var smtp = new SmtpClient();
+            smtp.Connect(_configuration["SMTP_HOST"], int.Parse(_configuration["SMTP_PORT"] ?? "465"));
+            smtp.Authenticate(_configuration["SMTP_USER"], _configuration["SMTP_PASSWORD"]);
+            var resp = smtp.Send(message);
+            smtp.Disconnect(true);
+        }
+        catch (Exception ex)
+        {
+            throw new HeadStartException("Error sending email", ex);
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task SendEmailAsync(string recipient, string subject, string htmlMessage)
+    {
+        return SendEmailAsync([recipient], subject, htmlMessage);
+    }
+
+
+}
