87a2f4883f6ea9052ef47f6aef79c9c2b46510a0
Robin Lenaerts <r.lenaerts@easi.net>
2024-03-28 12:42:42 +0100

    Update project configuration and add WebApplicationBuilder extension

---
+++ DotnetHeadStart.API/Extensions/WebApplicationBuilderExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace DotnetHeadStart.API;
+
+public static class WebApplicationBuilderExtension
+{
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+    {
+        //Add services to the DI container
+        return builder;
+    }
+}
--- DotnetHeadStart.API/Program.cs
+++ DotnetHeadStart.API/Program.cs
@@ -1,4 +1,8 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configManager = builder.Configuration;
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,20 +14,47 @@ builder.ConfigureLogging();
 
 Log.Error("Starting application");
 
+
+// Configure database mysql context
+var hostname = configManager["DB_HOSTNAME"];
+if (string.IsNullOrWhiteSpace(hostname))
+{
+    Log.Fatal("DB_HOSTNAME is not set. Please set it in the environment variables.");
+    throw new HeadStartException("DB_HOSTNAME is not set");
+}
+var port = configManager["DB_PORT"];
+if (string.IsNullOrWhiteSpace(port))
+{
+    Log.Information("DB_PORT not provided, using default 3306");
+    port = "3306";
+}
+var user = configManager["DB_USER"];
+if (string.IsNullOrWhiteSpace(user))
+{
+    Log.Fatal("DB_USER is not set. Please set it in the environment variables.");
+    throw new HeadStartException("DB_USER is not set");
+}
+var password = configManager["DB_PASSWORD"];
+if (string.IsNullOrWhiteSpace(password))
+{
+    Log.Fatal("DB_PASSWORD is not set. Please set it in the environment variables.");
+    throw new HeadStartException("DB_PASSWORD is not set");
+}
+
+var connectionstring = $"server={hostname};port={port};database=AITClient;user={user};password={password};convert zero datetime=True;Keepalive=120"; // mysql connection string
+
 var app = builder.Build();
 
 //catch global error handling
 app.UseExceptionHandler("/error"); // Replace "/error" with your desired error handling endpoint
 
 // error enpoint
-app.MapGet("/error", (ILogger logger, Exception e) =>
+app.MapGet("/error", (ILogger logger, HttpContext httpContext) =>
 {
-    logger.Error("An error occurred");
-    return Results.BadRequest($"An error occurred: {e.Message}");
+    var exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+    logger.Error(exception, $"An error occurred: \n {exception?.Message} \n {exception?.StackTrace}");
+    return Results.BadRequest($"An error occurred");
 });
-
-app.MapGet("/test", (e) => throw new NotImplementedException("Not implemented yet"));
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,7 +62,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseDefaultEndpoints(app.Configuration);
 app.MapHealthChecks("/health");
 app.MapControllers();
 
