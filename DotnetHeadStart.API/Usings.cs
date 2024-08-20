05619efcd1c0a9beb5f99930ef114c63b22a8695
Robin Lenaerts <contact@robinlenaerts.dev>
2024-08-20 18:27:49 +0200

    delete deprecated files

--- DotnetHeadStart.API/Usings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using DotnetHeadStart;
-global using Serilog;
-global using ILogger = Serilog.ILogger;
-global using Microsoft.AspNetCore.Diagnostics;
-global using Microsoft.AspNetCore.Mvc;
-global using DotnetHeadStart.API;
